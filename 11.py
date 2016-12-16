3df1bc9b7489a377a128657e153e2f5928cac37c
Jean-Loup <jean-loup@sprocket.bz>
2016-12-16 15:46:30 +0900

    Day 11 with tuples

--- 11.py
+++ 11.py
@@ -1,167 +1,110 @@
-from copy import deepcopy
+def possible_moves(state):
+    elevator_floor, floors = state
+
+    moves = set()
+
+    possible_deltas = []
+    if elevator_floor > 0:
+        possible_deltas.append(-1)
+    if elevator_floor < len(floors) - 1:
+        possible_deltas.append(1)
+
+    microchips, generators = floors[elevator_floor]
+
+    for possible_delta in possible_deltas:
+        for i in xrange(len(microchips)):
+            curr_microchip = microchips[i]
+            moves.add((possible_delta, (curr_microchip,), ()))
+
+            for j in xrange(i + 1, len(microchips)):
+                moves.add((possible_delta, (curr_microchip, microchips[j]), ()))
+
+            for generator in generators:
+                moves.add((possible_delta, (curr_microchip,), (generator,)))
+
+        for i in xrange(len(generators)):
+            curr_generator = generators[i]
+            moves.add((possible_delta, (), (curr_generator,)))
+
+            for j in xrange(i + 1, len(generators)):
+                moves.add((possible_delta, (), (curr_generator, generators[j])))
+
+    return moves
 
 
-class Floor(object):
-    def __init__(self, microchips, generators):
-        self.microchips = set(microchips)
-        self.generators = set(generators)
+def move_out(floor, microchips, generators):
+    return (
+        tuple(sorted(
+            microchip
+            for microchip in floor[0]
+            if microchip not in microchips
+        )),
+        tuple(sorted(
+            generator
+            for generator in floor[1]
+            if generator not in generators
+        ))
+    )
 
-    def __eq__(self, other):
-        return self.microchips == other.microchips and self.generators == other.generators
 
-    def __ne__(self, other):
-        return not self.__eq__(other)
+def move_in(floor, microchips, generators):
+    return (
+        tuple(sorted(floor[0] + microchips)),
+        tuple(sorted(floor[1] + generators))
+    )
 
-    def __repr__(self):
-        return "  Microchips: %s\n  Generators: %s" % (self.microchips, self.generators)
 
-    def is_valid(self):
-        for microchip in self.microchips:
-            if microchip in self.generators:
-                continue
-            if self.generators:
-                return False
+def move(state, move_params):
+    old_elevator_floor = state[0]
+    new_elevator_floor = old_elevator_floor + move_params[0]
+    microchips = move_params[1]
+    generators = move_params[2]
 
+    new_floors = []
+    for i, floor in enumerate(state[1]):
+        if i == old_elevator_floor:
+            new_floors.append(move_out(floor, microchips, generators))
+        elif i == new_elevator_floor:
+            new_floors.append(move_in(floor, microchips, generators))
+        else:
+            new_floors.append(floor)
+
+    return new_elevator_floor, tuple(new_floors)
+
+
+def is_valid_floor(floor):
+    generators = floor[1]
+
+    if not generators:
         return True
 
-    def is_empty(self):
-        return not(self.microchips or self.generators)
-
-    def move_in(self, microchips, generators):
-        for microchip in microchips:
-            self.microchips.add(microchip)
-        for generator in generators:
-            self.generators.add(generator)
-
-    def move_out(self, microchips, generators):
-        for microchip in microchips:
-            self.microchips.remove(microchip)
-        for generator in generators:
-            self.generators.remove(generator)
-
-
-class Building(object):
-    def __init__(self, floors):
-        self.floors = floors
-        self.elevator_floor = 0
-
-    def __eq__(self, other):
-        if self.elevator_floor != other.elevator_floor:
+    for microchip in floor[0]:
+        if microchip not in generators:
             return False
 
-        for own_floor, other_floor in zip(self.floors, other.floors):
-            if own_floor != other_floor:
-                return False
-
-        return True
-
-    def __ne__(self, other):
-        return not self.__eq__(other)
-
-    def __repr__(self):
-        return '\n'.join([
-            "[%d]%s\n%s" % (i, "<-" if self.elevator_floor == i else "", floor)
-            for i, floor in reversed(list(enumerate(self.floors)))
-        ])
-
-    def is_solved(self):
-        for i in xrange(len(self.floors) - 1):
-            if not self.floors[i].is_empty():
-                return False
-
-        return True
-
-    def is_valid(self):
-        for floor in self.floors:
-            if not floor.is_valid():
-                return False
-
-        return True
-
-    def move(self, delta, microchips, generators):
-        new_state = deepcopy(self)
-        new_state.floors[new_state.elevator_floor].move_out(microchips, generators)
-        new_state.elevator_floor += delta
-        new_state.floors[new_state.elevator_floor].move_in(microchips, generators)
-        return new_state
-
-    def possible_moves(self):
-        possible_moves = set()
-
-        possible_deltas = []
-        if self.elevator_floor > 0:
-            possible_deltas.append(-1)
-        if self.elevator_floor < len(self.floors) - 1:
-            possible_deltas.append(1)
-
-        current_floor = self.floors[self.elevator_floor]
-        microchips = list(current_floor.microchips)
-        generators = list(current_floor.generators)
-
-        for possible_delta in possible_deltas:
-            for i in xrange(len(microchips)):
-                curr_microchip = microchips[i]
-                possible_moves.add((possible_delta, (curr_microchip,), ()))
-
-                for j in xrange(i + 1, len(microchips)):
-                    possible_moves.add((possible_delta, (curr_microchip, microchips[j]), ()))
-
-                for generator in generators:
-                    possible_moves.add((possible_delta, (curr_microchip,), (generator,)))
-
-            for i in xrange(len(generators)):
-                curr_generator = generators[i]
-                possible_moves.add((possible_delta, (), (curr_generator,)))
-
-                for j in xrange(i + 1, len(generators)):
-                    possible_moves.add((possible_delta, (), (curr_generator, generators[j])))
-
-        return possible_moves
+    return True
 
 
-class StateSet(object):
-    pass
+def is_valid_state(state):
+    for floor in state[1]:
+        if not is_valid_floor(floor):
+            return False
+
+    return True
+
+
+def is_solved(state):
+    floors = state[1]
+
+    for i in xrange(0, len(floors) - 1):
+        floor = floors[i]
+        if floor[0] or floor[1]:
+            return False
+
+    return True
 
 
 def solve(initial_state):
-    state_history = {
-        initial_state: []
-    }
-    curr_states = [(initial_state, [])]
-
-    depth = 0
-    while True:
-        print "Depth: %d" % depth
-        print "Tree size: %d" % len(state_history.keys())
-
-        if not curr_states:
-            print "Total states: %d" % len(state_history.keys())
-            for state in state_history.keys():
-                print state
-                print "\n\n"
-            return None
-
-        next_states = []
-
-        for curr_state, move_history in curr_states:
-            for possible_move in curr_state.possible_moves():
-                new_state = curr_state.move(*possible_move)
-                if new_state.is_valid() and new_state not in state_history.keys():
-                    new_move_history = move_history + [possible_move]
-
-                    if new_state.is_solved():
-                        return new_move_history
-
-                    next_states.append((new_state, new_move_history))
-                    state_history[new_state] = new_move_history
-
-        print "Valid moves: %d" % len(next_states)
-        curr_states = next_states
-        depth += 1
-
-
-# TODO special set object for states
-def solve_v2(initial_state):
     state_history = {initial_state}
     curr_states = {initial_state}
 
@@ -176,10 +119,10 @@ def solve_v2(initial_state):
         next_states = set()
 
         for curr_state in curr_states:
-            for possible_move in curr_state.possible_moves():
-                new_state = curr_state.move(*possible_move)
-                if new_state.is_valid() and new_state not in state_history:
-                    if new_state.is_solved():
+            for possible_move in possible_moves(curr_state):
+                new_state = move(curr_state, possible_move)
+                if is_valid_state(new_state) and new_state not in state_history:
+                    if is_solved(new_state):
                         return depth + 1
 
                     next_states.add(new_state)
@@ -190,24 +133,28 @@ def solve_v2(initial_state):
         depth += 1
 
 
-building = Building([
-    Floor(
-        ['thulium', 'ruthenium', 'cobalt'],
-        ['polonium', 'thulium', 'promethium', 'ruthenium', 'cobalt']
-    ),
-    Floor(
-        ['polonium', 'promethium'],
-        []
-    ),
-    Floor(
-        [],
-        []
-    ),
-    Floor(
-        [],
-        []
+building = (
+    0,
+    (
+        (
+            ('Co', 'Ru', 'Tm'),
+            ('Co', 'Pm', 'Po', 'Ru', 'Tm')
+        ),
+        (
+            ('Pm', 'Po'),
+            ()
+        ),
+        (
+            (),
+            ()
+        ),
+        (
+            (),
+            ()
+        )
     )
-])
+)
+
 
 print building
-print solve_v2(building)
+print solve(building)
